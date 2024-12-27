4d8379e21cb882d80acfa5779895aa7c74a3711d
Alexander <al.tulin@gmail.com>
2024-12-27 08:39:07 +0800

    add aliases

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+
+
+
+const App =()=> {
+
+  return (
+    <>
+      <h1>321</h1>
+    </>
+  )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import autoprefixer from "autoprefixer";
+import { robots } from "vite-plugin-robots";
+// import createSvgSpritePlugin from "vite-plugin-svg-sprite";
+import tailwindcss from "tailwindcss";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr(),
+    robots({}),
+    // createSvgSpritePlugin({
+    //   symbolId: "icon-[name]-[hash]",
+    //   include: ["**/sprite/**.svg"],
+    // }),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}), // add options if needed
+        tailwindcss(),
+      ],
+    },
+    
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  base: "./",
+});
